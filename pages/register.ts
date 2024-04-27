3e2dc00ae4136c31d7e6e5ba916a6c9b8fbb346d
Rithik M <rithik7899@gmail.com>
2024-04-28 00:26:24 +0530

    addedhooks

--- pages/register.ts
+++ pages/register.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import { NextApiRequest, NextApiResponse } from 'next';
-import prismadb from '@/lib/prismadb';
+import prismadb from '@/lib/prismadb'
 import { NextResponse } from 'next/server';
 
 export async function POST(req : Request ) {
